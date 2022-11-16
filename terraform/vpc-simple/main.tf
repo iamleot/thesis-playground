a2728bdd7fdfc7fb24a79b5675b08449e50956bd
Leonardo Taccari <iamleot@gmail.com>
2022-11-16 22:44:05 +0100

    Add vpc-simple live module
    Add a simple live module that creates a VPC on AWS.
    
    Most of the code was shamelessy copypasted from
    terraform-aws-modules/vpc/aws examples.
    
    The AWS provider adjustments are based on official LocalStack
    documentation <https://docs.localstack.cloud/integrations/terraform/>.
    
    Intended to be used with LocalStack or Moto (i.e. mocking AWS just to
    be able to do a `plan' / `apply' and possibly inspect the state without
    worrying on managing such resources on real AWS accounts).

---
+++ terraform/vpc-simple/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_version = "~> 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  s3_force_path_style         = false
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    apigatewayv2   = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+    es             = "http://localhost:4566"
+    elasticache    = "http://localhost:4566"
+    firehose       = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    rds            = "http://localhost:4566"
+    redshift       = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    s3             = "http://s3.localhost.localstack.cloud:4566"
+    secretsmanager = "http://localhost:4566"
+    ses            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    ssm            = "http://localhost:4566"
+    stepfunctions  = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = "vpc-simple"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+
+  tags = {
+    Project   = "vpc-simple"
+    Terraform = "true"
+  }
+}
