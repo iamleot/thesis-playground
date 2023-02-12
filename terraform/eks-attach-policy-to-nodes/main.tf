4ce90b108f5223c3d7ab6f029ed88ea2ad12686d
Leonardo Taccari <iamleot@gmail.com>
2023-02-12 13:06:18 +0100

    Add a self-managed node group
    Add a node group with 6 nodes so that EKS examples are actually much
    more real now (a cluster without any worker nodes is not very useful
    usually!).

--- terraform/eks-attach-policy-to-nodes/main.tf
+++ terraform/eks-attach-policy-to-nodes/main.tf
@@ -60,6 +60,17 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = local.cluster_version
 
+  self_managed_node_groups = {
+    eks_attach_policy_to_nodes = {
+      instance_type = "t3.xlarge"
+
+      min_size     = 6
+      max_size     = 6
+      desired_size = 6
+    }
+  }
+
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 }
