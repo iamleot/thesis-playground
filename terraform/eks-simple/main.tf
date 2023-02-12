1c8b64de4b7fe32c6963ee82f11ebccc114e464d
Leonardo Taccari <iamleot@gmail.com>
2023-02-12 12:16:10 +0100

    Switch to eu-central-1 everywhere
    AZs of VPC were accidentally missed in commit
    a8eeb5f61aea23c5289ec9e4b782c4cddc324239.

--- terraform/eks-simple/main.tf
+++ terraform/eks-simple/main.tf
@@ -71,7 +71,7 @@ module "vpc" {
   name = "vpc-simple"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
